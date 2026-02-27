7887783c3d37279410d0bf49b21395fd40f94c0b
andrewleachtx <andrewleach38@gmail.com>
2026-02-26 18:56:38 -0800

    Write kernel and test against basic solve

--- src/spmm.cu
+++ src/spmm.cu
@@ -1,26 +1,45 @@
 #include "gpu_array.hpp"
 #include "sparse_csr.h"
+#include <cstring>
 #include <iostream>
 
-constexpr int M = 1 << 10;
-constexpr int K = 1 << 10;
-constexpr int N = 1 << 10;
+#define WARP_SZ 32
 
 /*
 Given an m-by-k sparse matrix A and a k-by-n dense matrix B, SpMM computes
 an m-by-n dense matrix C = AB.
-
-TODO: B, C are dense, so we can just use a float* but A needs a CSR format
-    - A: i[], j[], k[] can be size_t*, size_t*, float*
-
 */
-__global__ void spmm_kernel(size_t* i, size_t* j, float* k, size_t nnz,
-                            float* B, float* C) {}
+__global__ void spmm_kernel(const int* row_ptr, const int* col_ptr,
+                            const float* val_ptr, const size_t nnz,
+                            const float* B, float* C, const size_t M,
+                            const size_t N) {
+    int lane = threadIdx.x % WARP_SZ;
+    int per_blk_warp = threadIdx.x / WARP_SZ;
+    int warps_per_blk = blockDim.x / WARP_SZ;
+
+    const int row = blockIdx.x * warps_per_blk + per_blk_warp;
+    const int col = blockIdx.y * WARP_SZ + lane;
+
+    if (row >= M || col >= N) {
+        return;
+    }
+
+    const int row_size = row_ptr[row + 1] - row_ptr[row];
+
+    // not paper optimized yet
+    float acc = 0.0f;
+    const int x = row_ptr[row];
+    for (int i = 0; i < row_size; i++) {
+        int k = col_ptr[x + i];
+        float a_val = val_ptr[x + i];
+        float b_val = B[k * N + col];
+        acc += a_val * b_val;
+    }
+
+    C[row * N + col] = acc;
+}
 
 int main() {
-    // these guys are flattened, accessing B[r][c] should be B[r * N + c] and C[r][c] should also be C[r * N + c]
-    fun::gpu_array<float> B(K * N);
-    fun::gpu_array<float> C(M * N);
 
     // TODO(O): populate i, j, k, A, B, C and get nnz?
 
@@ -30,31 +49,64 @@ int main() {
     Eigen::MatrixXf B_dense(3, 3);
     B_dense << 1, 2, 3, 4, 5, 6, 7, 8, 9;
 
+    const int M = A.rows();
+    const int K = A.cols();
+    const int N = B_dense.cols();
+
     CSR A_csr = sparse_to_CSR(dense_to_sparse(A));
-    int M = A_csr.i.size();
     int nnz = A_csr.j.size();
 
-    fun::gpu_array<size_t> i(M);
-    fun::gpu_array<size_t> j(nnz);
+    // these guys are flattened, accessing B[r][c] should be B[r * N + c] and C[r][c] should also be C[r * N + c]
+    fun::gpu_array<float> B(K * N);
+    fun::gpu_array<float> C(M * N);
+
+    fun::gpu_array<int> i(M + 1);
+    fun::gpu_array<int> j(nnz);
     fun::gpu_array<float> k(nnz);
 
-    // A_csr.i.data()
-    // A_csr.j.data()
-    // A_csr.k.data()
-    // B_dense.data()
+    std::memcpy(&i[0], A_csr.i.data(), (M + 1) * sizeof(int));
+    std::memcpy(&j[0], A_csr.j.data(), nnz * sizeof(int));
+    std::memcpy(&k[0], A_csr.k.data(), nnz * sizeof(float));
+
+    // B_dense is column-major; kernel expects row-major
+    Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+        B_row = B_dense;
+    std::memcpy(&B[0], B_row.data(), K * N * sizeof(float));
+
+    /*
+        - The amount of warps per block is arbitrary, really just used for threads per block (which is also a hyperparameter)
+        - blockIdx.x selects a tile per row
+            - then gridDim.x = ceil(M / warps_per_blk)
+        - blockIdx.y selects a tile per 32 columns
+            - then gridDim.y = ceil(N / 32)
+            
+        Together, the 2D grid gives us for each x block: multiple y blocks or chunks, of size cols / warp size
+    */
+    const int warps_per_blk = 16;
+    const int threads_per_blk = warps_per_blk * WARP_SZ;
+    dim3 blks_per_grid {
+        // ceil(a / b) == (a + b - 1) / b
+        static_cast<uint32_t>((M + warps_per_blk - 1) / warps_per_blk),
+        static_cast<uint32_t>((N + WARP_SZ - 1) / WARP_SZ), 1
+    };
 
     // send to GPU
     fun::to_device_all(i, j, k, B);
 
-    spmm_kernel<<<67, 67>>>(i.device_ptr(), j.device_ptr(), k.device_ptr(), nnz,
-                            B.device_ptr(), C.device_ptr());
+    spmm_kernel<<<blks_per_grid, threads_per_blk>>>(
+        i.device_ptr(), j.device_ptr(), k.device_ptr(), nnz, B.device_ptr(),
+        C.device_ptr(), M, N);
 
     C.to_host();
 
-    printf("hello worlds\n");
-
     // random_dense(4);
-    test();
+    // test();
     // TODO: compare to some other library or check output i guess
-    //   std::cout << "\nEigen answer\n" << A*B_dense << std::endl;
+    Eigen::Map<
+        Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>
+        C_eigen(&C[0], M, N);
+
+    Eigen::MatrixXf ref = A * B_dense;
+    std::cout << "CUDA:\n" << C_eigen << "\n";
+    std::cout << "Eigen:\n" << ref << "\n";
 }
